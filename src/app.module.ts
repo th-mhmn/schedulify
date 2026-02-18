eeced2c592a4f02878980eeda9107b0e0e2e329c
Taha <tahamehmandoust@gmail.com>
2026-02-18 20:19:33 +0330

    feat(businesses): add CRUD endpoints for businesses

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { BusinessesModule } from './businesses/businesses.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule, BusinessesModule],
   controllers: [AppController],
   providers: [AppService],
 })
