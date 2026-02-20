4e2f18b409ac6e60f177e51709b7088e731d0323
Taha <tahamehmandoust@gmail.com>
2026-02-20 13:55:08 +0330

    chore(prisma): add prisma service

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaClient } from './generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
+      connectionString: process.env.DATABASE_URL as string,
     });
     super({ adapter });
   }
