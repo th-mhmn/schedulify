dd4b885b54c4738cc78a2d552093327167feb1dd
Taha <tahamehmandoust@gmail.com>
2026-02-20 23:23:15 +0330

    chore(auth): configure jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '@/users/users.module';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>('JWT_ACCESS_SECRET'),
+        signOptions: { expiresIn: '15m' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
