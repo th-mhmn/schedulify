fcedf930e385766f13814aa635827b158d40832e
Taha <tahamehmandoust@gmail.com>
2026-02-20 23:24:32 +0330

    feat(auth): add auth endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { TransformDTO } from '../../_core/interceptors/transform-dto.interceptor';
+import { ResponseUserDto } from '@/users/dto/response-user.dto';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Controller('auth')
+@TransformDTO(ResponseUserDto)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('sign-up')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
+
+  @Post('sign-in')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
+  }
 }
