dc5d4b9f74f40eb4ee86adfcfdade75bc2bbce31
Taha <tahamehmandoust@gmail.com>
2026-02-21 19:44:50 +0330

    chore(app): add versioning to requests url (api/v1)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: '1',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
