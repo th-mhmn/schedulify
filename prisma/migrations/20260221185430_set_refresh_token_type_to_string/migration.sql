b57d1a8e7d16a52c4bba00db2dd4b25a5f09b84a
Taha <tahamehmandoust@gmail.com>
2026-02-22 14:35:36 +0330

    chore(prisma): set refresh token type to string

---
+++ prisma/migrations/20260221185430_set_refresh_token_type_to_string/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the `RefreshToken` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `refreshToken` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "RefreshToken" DROP CONSTRAINT "RefreshToken_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "refreshToken" TEXT NOT NULL;
+
+-- DropTable
+DROP TABLE "RefreshToken";
