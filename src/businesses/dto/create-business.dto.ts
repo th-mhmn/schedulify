c24a583adc0bd25a6f4f4cb10c7ca38c51e03b49
taha <tahamehmandoust@gmail.com>
2026-02-23 09:26:24 +0330

    feat(docs): add swagger for business endpoints

--- src/businesses/dto/create-business.dto.ts
+++ src/businesses/dto/create-business.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsTimeZone } from 'class-validator';
 
 export class CreateBusinessDto {
+  @ApiProperty({ example: 'Acme Inc' })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({ example: 'Asia/Baku' })
   @IsNotEmpty()
   @IsTimeZone()
   timezone: string;
