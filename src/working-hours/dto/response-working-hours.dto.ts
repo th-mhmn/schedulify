50b842c9bdb67f3e38cbcb8d3c7ba7dba9b0a12b
Taha <tahamehmandoust@gmail.com>
2026-02-25 23:04:35 +0330

    chore(working-hours): add response dto

---
+++ src/working-hours/dto/response-working-hours.dto.ts
@@ -0,0 +1,43 @@
+import { Expose, Transform } from 'class-transformer';
+
+export class ResponseWorkingHoursDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  businessId: number;
+
+  @Expose()
+  @Transform(({ obj }) => convertDays(obj.dayOfWeek))
+  dayOfWeek: string;
+
+  @Expose()
+  startTime: string;
+
+  @Expose()
+  endTime: string;
+
+  @Expose()
+  createdAt: string;
+}
+
+function convertDays(index: number) {
+  switch (index) {
+    case 0:
+      return 'Monday';
+    case 1:
+      return 'Tuesday';
+    case 2:
+      return 'Wednesday';
+    case 3:
+      return 'Thursday';
+    case 4:
+      return 'Friday';
+    case 5:
+      return 'Saturday';
+    case 6:
+      return 'Sunday';
+    default:
+      return 'N/A';
+  }
+}
