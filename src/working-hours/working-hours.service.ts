fad9a519eeda6da86471047ead09cf1b9b3c6888
Taha <tahamehmandoust@gmail.com>
2026-02-25 23:45:02 +0330

    refactor(working-hours): wrap queries in prisma transaction

--- src/working-hours/working-hours.service.ts
+++ src/working-hours/working-hours.service.ts
@@ -8,14 +8,18 @@ export class WorkingHoursService {
   constructor(private readonly prisma: PrismaService) {}
 
   async setWeeklySchedule(dto: WeekScheduleDto, businessId: number) {
-    await this.prisma.workingHours.deleteMany({ where: { businessId } });
-    const payload = {
-      days: dto.days.map((d) => ({ ...d, businessId })),
-    };
-    const weeklySchedule = await this.prisma.workingHours.createManyAndReturn({
-      data: payload.days,
+    const res = await this.prisma.$transaction(async (tx) => {
+      await tx.workingHours.deleteMany({ where: { businessId } });
+      const payload = {
+        days: dto.days.map((d) => ({ ...d, businessId })),
+      };
+
+      const weeklySchedule = await tx.workingHours.createManyAndReturn({
+        data: payload.days,
+      });
+      return _.sortBy(weeklySchedule, 'dayOfWeek');
     });
-    return _.sortBy(weeklySchedule, 'dayOfWeek');
+    return res;
   }
 
   async getWeeklySchedule(businessId: number) {
