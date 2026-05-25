7fa9fc21e475aa5042f9554c30ed5365e8d2c3bb
Taha <tahamehmandoust@gmail.com>
2026-05-25 10:03:57 +0330

    fix(docs): add prefix to swagger v1 docs

--- src/main.ts
+++ src/main.ts
@@ -53,7 +53,7 @@ async function bootstrap() {
     })
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api/v1/docs', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
