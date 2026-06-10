cd3525ec0cbe22e163e5192ff070dbe32893d2d2
Taha <tahamehmandoust@gmail.com>
2026-06-10 12:42:33 +0330

    chore(app): add throttler behind proxy guard

---
+++ src/_core/guards/throttler-behind-proxy.guard.ts
@@ -0,0 +1,10 @@
+// src/common/guards/throttler-behind-proxy.guard.ts
+import { ThrottlerGuard } from '@nestjs/throttler';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
+  protected async getTracker(req: Record<string, any>): Promise<string> {
+    return req.ips.length ? req.ips[0] : req.ip;
+  }
+}
