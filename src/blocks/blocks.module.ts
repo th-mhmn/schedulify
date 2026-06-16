1008ed00cfb067e028c890b5a15db5f187fd5902
Taha <tahamehmandoust@gmail.com>
2026-06-16 16:11:14 +0330

    refactor(blocks): extract separate functions

--- src/blocks/blocks.module.ts
+++ src/blocks/blocks.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { BlocksService } from './blocks.service';
-import { BlocksController } from './blocks.controller';
 import { PrismaService } from '@/prisma.service';
+import { Module } from '@nestjs/common';
+import { BlocksController } from './blocks.controller';
+import { BlocksService } from './blocks.service';
+import { BlocksOverlapService } from './validators/blocks-overlap.validator';
 
 @Module({
   controllers: [BlocksController],
-  providers: [BlocksService, PrismaService],
+  providers: [BlocksService, PrismaService, BlocksOverlapService],
 })
 export class BlocksModule {}
