41f1ef1499ddeb4c0b796e3d894bbf69800bfd7b
Taha <tahamehmandoust@gmail.com>
2026-06-17 09:33:01 +0330

    refactor(blocks): extract separate functions

--- src/businesses/businesses.module.ts
+++ src/businesses/businesses.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
-import { BusinessesService } from './businesses.service';
-import { BusinessesController } from './businesses.controller';
+import { IsoRangeValidator } from '@/_core/validators/iso-range.validator';
+import { TimeRangeValidator } from '@/_core/validators/time-range.validator';
+import { BlocksService } from '@/blocks/blocks.service';
+import { BookingsModule } from '@/bookings/bookings.module';
 import { PrismaService } from '@/prisma.service';
 import { ServicesService } from '@/services/services.service';
 import { WorkingHoursService } from '@/working-hours/working-hours.service';
-import { TimeRangeValidator } from '@/_core/validators/time-range.validator';
-import { BlocksService } from '@/blocks/blocks.service';
-import { IsoRangeValidator } from '@/_core/validators/iso-range.validator';
-import { BookingsModule } from '@/bookings/bookings.module';
+import { Module } from '@nestjs/common';
+import { BusinessesController } from './businesses.controller';
+import { BusinessesService } from './businesses.service';
+import { BusinessValidator } from './validators/business.validator';
 
 @Module({
   imports: [BookingsModule],
@@ -20,6 +21,7 @@ import { BookingsModule } from '@/bookings/bookings.module';
     TimeRangeValidator,
     IsoRangeValidator,
     BlocksService,
+    BusinessValidator,
   ],
 })
 export class BusinessesModule {}
