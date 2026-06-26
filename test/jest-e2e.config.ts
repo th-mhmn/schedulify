ab6a5a8302136cd29a0ed90a958cabd6d87be684
Taha <tahamehmandoust@gmail.com>
2026-06-26 10:11:51 +0330

    chore(test): setup e2e test scripts & config

---
+++ test/jest-e2e.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  rootDir: '..',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.e2e.spec\\.ts$',
+  transform: { '^.+\\.ts$': 'ts-jest' },
+  testEnvironment: 'node',
+  modulePaths: ['<rootDir>'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
+
+export default config;
