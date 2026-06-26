d7641cc8329fd6896e3888c564f26d4b572404dd
Taha <tahamehmandoust@gmail.com>
2026-06-26 12:46:12 +0330

    chore(business): add min length to business name dto

--- src/businesses/dto/create-business.dto.ts
+++ src/businesses/dto/create-business.dto.ts
@@ -1,8 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsTimeZone } from 'class-validator';
+import { IsNotEmpty, IsString, IsTimeZone, MinLength } from 'class-validator';
 
 export class CreateBusinessDto {
   @ApiProperty({ example: 'Acme Inc' })
+  @MinLength(4)
   @IsNotEmpty()
   @IsString()
   name: string;
