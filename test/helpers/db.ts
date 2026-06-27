5d26d86a804585c77af055ca34cefe51527b01a2
Taha <tahamehmandoust@gmail.com>
2026-06-27 19:38:22 +0330

    chore(test): clear blocks table before tests

--- test/helpers/db.ts
+++ test/helpers/db.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client/extension';
 
 export async function cleanDatabase(prisma: PrismaClient) {
   await prisma.booking.deleteMany();
+  await prisma.availabilityBlock.deleteMany();
   await prisma.workingHours.deleteMany();
   await prisma.service.deleteMany();
   await prisma.business.deleteMany();
